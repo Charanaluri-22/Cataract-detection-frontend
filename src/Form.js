a88777b0419964d2e9bfe1ac7a9703cd7bdbfebb
aluri charan <aluri1729charan@gmail.com>
2026-06-20 12:52:58 +0530

    updated dynamic backend url and favicon

--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import { Box, Button, TextField, Typography, Grid, Container } from '@mui/material';
 
@@ -7,12 +7,15 @@ const Form = () => {
     const [predictionResult, setPredictionResult] = useState(null);
     const [imagePreview, setImagePreview] = useState(null);
     const [errorMessage, setErrorMessage] = useState(null);
+    const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
+    const resultRef = useRef(null);
+    const errorRef = useRef(null);
 
     const handleFileChange = (event) => {
         setSelectedFile(event.target.files[0]);
         setPredictionResult(null); // Reset prediction result
-        // Preview the image
+        setErrorMessage(null); // Reset error
         setImagePreview(URL.createObjectURL(event.target.files[0]));
     };
 
@@ -28,86 +31,112 @@ const Form = () => {
         formData.append('file', selectedFile);
 
         try {
-            const response = await axios.post(`http://localhost:5000/predict`, formData, {
+            const response = await axios.post(`${backendUrl}/predict`, formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
             });
             setPredictionResult(response.data);
-            setErrorMessage(null); // Clear any previous error
+            setErrorMessage(null);
+
+            setTimeout(() => {
+                resultRef.current?.scrollIntoView({ behavior: 'smooth' });
+            }, 100);
         } catch (error) {
+            setPredictionResult(null);
+
             if (error.response && error.response.data && error.response.data.error) {
-                setErrorMessage(error.response.data.error); // Set backend error message
+                setErrorMessage(error.response.data.error);
             } else {
-                setErrorMessage("Something went wrong during prediction."); // Generic error
+                setErrorMessage("Something went wrong during prediction.");
                 console.error("Error during image prediction:", error);
             }
-        }
 
+            setTimeout(() => {
+                errorRef.current?.scrollIntoView({ behavior: 'smooth' });
+            }, 100);
+        }
     };
 
     return (
-        <>
-            <Container component="main" maxWidth="sm" className="box">
-                <Typography variant="h4" align="center">Cataract Detection</Typography>
-                <Box
-                    component="form"
-                    onSubmit={handleSubmit}
-                    noValidate
-                    sx={{ mt: 1 }}
-                >
-                    <Typography variant="h6">Upload Image</Typography>
-                    <Grid container spacing={2}>
-                        <Grid item xs={12}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                type="file"
-                                onChange={handleFileChange}
-                                inputProps={{ accept: "image/*" }}
-                            />
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Predict
-                            </Button>
-                        </Grid>
+        <Container component="main" maxWidth="sm" className="box">
+            <Typography variant="h4" align="center">Cataract Detection</Typography>
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
+                noValidate
+                sx={{ mt: 1 }}
+            >
+                <Typography variant="h6">Upload Image</Typography>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            type="file"
+                            onChange={handleFileChange}
+                            inputProps={{ accept: "image/*" }}
+                        />
                     </Grid>
-                    {imagePreview && (
-                        <Box sx={{ mt: 2, textAlign: 'center' }}>
-                            <img src={imagePreview} alt="Preview" style={{ maxWidth: '100%', maxHeight: '300px' }} />
-                        </Box>
-                    )}
-                    {predictionResult && (
-                        <Box sx={{ mt: 2, p: 2, border: '1px solid #ddd', borderRadius: '4px', backgroundColor: '#f5f5f5' }}>
-                            <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#3f51b5' }}>
-                                Prediction Result
-                            </Typography>
-                            <Typography variant="subtitle1" sx={{ mt: 1 }}>
-                                Class: <span style={{ fontWeight: 'bold', color: '#4caf50' }}>{predictionResult.class}</span>
-                            </Typography>
-                            <Typography variant="subtitle1">
-                                Confidence: <span style={{ fontWeight: 'bold', color: '#f44336' }}>{(predictionResult.confidence_score * 100).toFixed(2)}%</span>
-                            </Typography>
-                        </Box>
-                    )}~
-                    {errorMessage && (
-                        <Box sx={{ mt: 2, p: 2, border: '1px solid #ffcccc', borderRadius: '4px', backgroundColor: '#ffe6e6' }}>
-                            <Typography variant="subtitle1" sx={{ color: '#d32f2f', fontWeight: 'bold' }}>
-                                Error: {errorMessage}
-                            </Typography>
-                        </Box>
-                    )}
-                </Box>
-            </Container>
-
-        </>
+                    <Grid item xs={12}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Predict
+                        </Button>
+                    </Grid>
+                </Grid>
+                {imagePreview && (
+                    <Box sx={{ mt: 2, textAlign: 'center' }}>
+                        <img src={imagePreview} alt="Preview" style={{ maxWidth: '100%', maxHeight: '300px' }} />
+                    </Box>
+                )}
+                {predictionResult && (
+                    <Box
+                        ref={resultRef}
+                        sx={{
+                            mt: 2,
+                            p: 2,
+                            border: '1px solid #ddd',
+                            borderRadius: '4px',
+                            backgroundColor: '#f5f5f5'
+                        }}
+                    >
+                        <Typography variant="h6" sx={{ fontWeight: 'bold', color: '#3f51b5' }}>
+                            Prediction Result
+                        </Typography>
+                        <Typography variant="subtitle1" sx={{ mt: 1 }}>
+                            Class: <span style={{ fontWeight: 'bold', color: '#4caf50' }}>{predictionResult.class}</span>
+                        </Typography>
+                        <Typography variant="subtitle1">
+                            Confidence: <span style={{ fontWeight: 'bold', color: '#f44336' }}>
+                                {(predictionResult.confidence_score * 100).toFixed(2)}%
+                            </span>
+                        </Typography>
+                    </Box>
+                )}
+                {errorMessage && (
+                    <Box
+                        ref={errorRef}
+                        sx={{
+                            mt: 2,
+                            p: 2,
+                            border: '1px solid #ffcccc',
+                            borderRadius: '4px',
+                            backgroundColor: '#ffe6e6'
+                        }}
+                    >
+                        <Typography variant="subtitle1" sx={{ color: '#d32f2f', fontWeight: 'bold' }}>
+                            Error: {errorMessage}
+                        </Typography>
+                    </Box>
+                )}
+            </Box>
+        </Container>
     );
 };
 
